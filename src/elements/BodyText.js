07f004b5550138e66ac2d27d0ad98191613e976a
roy white <letsgroove@roynoMacBook-Air.local>
2019-05-28 23:48:54 +0900

    BodyTex.js

---
+++ src/elements/BodyText.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class BodyText extends 'React.Component' {
+  render() {
+    return (
+      <View>
+        <Text style={styles.text}>Hello!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#d00',
+    backgroundColor: '#eee',
+  },
+});
+
+export default BodyText;
