82042f3ac4516b1c5e2b108a8a9ad81e5e5ebd81
roy white <letsgroove@roynoMacBook-Air.local>
2019-06-02 02:53:32 +0900

    Create MemoList

--- src/elements/BodyText.js
+++ src/elements/BodyText.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-class BodyText extends 'React.Component' {
+class BodyText extends React.Component {
   render() {
     return (
       <View>
-        <Text style={styles.text}>Hello!</Text>
+        <Text style={styles.text}>
+          (this.props.children)
+        </Text>
       </View>
     );
   }
